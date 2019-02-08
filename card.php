d53da88110ea9667c86a5c91b0dee00fa5a4bdca
isaac <mengue.quentin@gmail.com>
2019-02-08 17:19:11 +0000

    Test de luhn validé

--- card.php
+++ card.php
@@ -1,3 +1,28 @@
+<?php
+    namespace credit;
+    
+    require 'Luhn.php';
+    if(isset($_POST['nombre'])){
+        if(!preg_match("#^[0-9]{16}#",$_POST['nombre'])){
+            echo "Le format <strong>".$_POST['nombre']."</strong> est invalide vous devez rentrer un nombre à 16 chiffres";
+        }
+        if(strlen($_POST['nombre']) > 16){
+            echo "Vous devez rentrer un nombre à 16 chiffre";
+        }
+        if(preg_match("#^[0]{16,16}$#",$_POST['nombre'])){
+            echo "Bien tenté mais le format avec seize zéro ne passe pas";
+        }else{
+            $nombre = $_POST['nombre'];
+            $luhn = new Luhn();
+            if($luhn->validate($nombre) == true){
+                echo "Le numéro de carte est valide";
+            }else{
+                echo "Le numéro de carte n'est pas valide";
+            }
+        }
+        
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +33,7 @@
         <p>
             <fieldset style="text-align:center">
                 <legend>Vérification du numéro de votre carte bancaire</legend>
-                <form action="carte.php" method=POST>
+                <form action="card.php" method=POST>
                     <label for="nombre">Numéro carte :</label><br/>
                     <input type="text" name="nombre"><br/>
                     <input type="submit" value="Validé">
@@ -17,13 +42,3 @@
         </p>
     </body>
 </html>
-<?php
-    if(isset($_POST['nombre'])){
-        if(!preg_match("#^[0-9]{16}#",$_POST['nombre'])){
-            echo "Le format <strong>".$_POST['nombre']."</strong> est invalide vous devez rentrer un nombre à 16 chiffres";
-        }
-        if(strlen($_POST['nombre']) > 16){
-            echo "Vous devez rentrer un nombre à 16 chiffre";
-        }
-    }
-?>
