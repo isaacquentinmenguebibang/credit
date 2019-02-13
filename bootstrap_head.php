ac9a44cef1652b2dc8aba49ec5648987767fb938
isaac <mengue.quentin@gmail.com>
2019-02-13 17:34:29 +0000

    Ajoute du css bootstrap

--- bootstrap_head.php
+++ bootstrap_head.php
@@ -1,1 +1,38 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<style type="text/css">
+      body { background-color:#D5D5D4; }
+      [class*="col"] { margin-bottom: 30px; }
+      
+      .well {
+        background-color:#4aa4b9;
+        padding: 15px;
+        margin-top: 15px;
+      }
+      img{
+          width: 70%;
+      }
+      .navbar-inverse {
+         background-color: #cccccc;
+        border-color: #080808;
+        }
+        .navbar-nav {
+             margin: 7.5px -15px;
+        }
+        .nav > li {
+            position: relative;
+            display: block;
+
+        }
+        .navbar-inverse .navbar-nav > li > a {
+            color: #ffffff;
+        }
+        .navbar-inverse .navbar-nav > h3 {
+            color: #31708f;
+        }
+        h1{
+            text-align: center;
+        }
+        .navbar-default .navbar   ul li a{
+            color: #ffffff;
+        }
+    </style>
--- card.php
+++ card.php
@@ -2,39 +2,54 @@
     namespace credit;
     
     require 'Luhn.php';
-
-    if(isset($_POST['nombre'])){
-        if(!preg_match("#^[0-9]{16}$#",$_POST['nombre'])){
-            echo "Le format <strong>".$_POST['nombre']."</strong> est invalide vous devez rentrer un nombre à 16 chiffres";
-        }elseif(preg_match("#^[0]{16}$#",$_POST['nombre'])){
-            echo "Bien tenté mais le format avec seize zéro ne passe pas";
-        }else{
-            $nombre = $_POST['nombre'];
-            $luhn = new Luhn();
-            if($luhn->validate($nombre) == true){
-                echo "Le numéro de carte est valide";
-            }else{
-                echo "Le numéro de carte n'est pas valide";
-            }
-        } 
-    }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Vérification de la validité de votre carte</title>
         <meta charset="uft-8"/>
+        <?php include('bootstrap_head.php'); ?>
     </head>
     <body>
-        <p>
-            <fieldset style="text-align:center">
-                <legend>Vérification du numéro de votre carte bancaire</legend>
-                <form action="card.php" method=POST>
-                    <label for="nombre">Numéro carte :</label><br/>
-                    <input type="text" name="nombre"><br/>
-                    <input type="submit" value="Validé">
-                </form>
-            </fieldset>
-        </p>
+        <div class="container">
+            <header class="row">
+                <div class="col-md-12">
+                    <h1 class="jumbotron"> Vérification de la validité de votre carte</h1>
+                    
+                </div>
+            </header>
+            <div class="row">
+                <menu class="col-md-3">
+                    <?php include('menu.php'); ?>
+                </menu>
+                <section class="col-md-6">
+                    <fieldset>
+                        <form action="card.php" method=POST class="well">
+                            <label for="nombre">Numéro carte :</label>
+                            <input type="text" name="nombre" class="form-control"><br>
+                            <button type="submit" class="btn btn-primary">Vérifier</button>
+                        </form>
+                    </fieldset>
+                </section>
+            </div>
+        </div>
+        <?php include('bootstrap_end.php'); ?>
     </body>
 </html>
+<?php
+        if(isset($_POST['nombre'])){
+            if(!preg_match("#^[0-9]{16}$#",$_POST['nombre'])){
+                echo "Le format <strong>".$_POST['nombre']."</strong> est invalide vous devez rentrer un nombre à 16 chiffres";
+            }elseif(preg_match("#^[0]{16}$#",$_POST['nombre'])){
+                echo "Bien tenté mais le format avec seize zéro ne passe pas";
+            }else{
+                $nombre = $_POST['nombre'];
+                $luhn = new Luhn();
+                if($luhn->validate($nombre) == true){
+                    echo "Le numéro de carte est valide";
+                }else{
+                    echo "Le numéro de carte n'est pas valide";
+                }
+            } 
+        }
+?>
